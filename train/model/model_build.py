2ccdad0f48897ebf8b1d70ffc8bd0a546aa28723
wangfugui <814400404@qq.com>
2023-03-22 15:11:24 +0000

    initial commit. Capture and basic model ok

---
+++ train/model/model_build.py
@@ -0,0 +1,90 @@
+# 
+#  Created by hwx on 2021/12/15
+# 
+
+import torch
+from torch import nn
+
+
+# from model.backbone.mobilenetv2 import *
+# from model.backbone.mobilenetv2_diy import *
+# from model.backbone.alexnet import AlexNet
+from model.backbone.alexnet import AlexNet
+from model.backbone.resnet import ResNet, BasicBlock
+from collections import OrderedDict
+
+
+
+class class_model(nn.Module):
+    def __init__(self, backbone, classifier, class_num, loss=None, is_training=True):
+        super(class_model, self).__init__()
+        self.backbone = backbone
+        self.classifier = classifier
+        self.is_training = is_training
+        self.loss = loss
+        self.class_num = class_num
+        return;
+
+
+    def forward(self, data, label=None):
+        if self.is_training:
+            shape = data[0].shape
+            feature = self.backbone(data[0])
+        else:
+            shape = data.shape
+            feature = self.backbone(data)
+        pred = self.classifier(feature)
+
+        if not self.is_training:
+            return pred
+        else:
+            l = self.loss(pred, data[1])
+            return l
+
+
+def class_loss(pred_obj, label_obj, training_mask=None, select_label=None):
+    return torch.nn.functional.nll_loss(torch.nn.functional.log_softmax(pred_obj, dim=1), label_obj)
+
+
+def build(args=None):
+    if args is None or args.backbone == 'alexnet':
+        backbone = AlexNet(args.class_num);
+        classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, args.class_num),
+        )
+    elif args.backbone == 'resnet18':
+        backbone = ResNet(BasicBlock, [2, 2, 2, 2], args.class_num)
+        classifier = nn.Sequential(
+            OrderedDict([
+                ('fc', nn.Linear(512 * BasicBlock.expansion, args.class_num))
+            ])
+        )
+    
+    m = class_model(backbone, classifier, args.class_num, class_loss, not args.eval);
+
+    if args.pretrained_model:
+        pretrained_dict = torch.load(args.pretrained_model)
+        m.backbone.load_state_dict(pretrained_dict, strict=False)
+
+    m.to(args.device)
+    return m
+
+if __name__ == '__main__':
+    # backbone = mobilenet_v2(used_layers=[1, 2, 3, 4, 5]);
+    # m = seg_model(backbone, 4, seg_loss.seg_loss)
+    # for p in m.parameters():
+    #     print(p.shape)
+
+    # backbone = alexnet(True, model_root)
+    backbone = AlexNet(3);
+    m = class_model(backbone, 3, class_loss)
+    for p in m.parameters():
+        print(p.shape)
+
+    print(m)
---
+++ train/train.py
@@ -0,0 +1,158 @@
+# 
+#  Created by hwx on 2021/12/16
+# 
+
+import argparse
+
+import os
+import os.path as osp
+import time
+
+import torch
+from torch.utils.data import DataLoader
+import datetime
+
+from model import model_build
+from dataset import data_reader
+from utils import logger
+
+ts = time.strftime("%Y%m%d_%H%M", time.localtime())
+os.makedirs('./output/log/', exist_ok=True)
+logger = logger.get_logger(filename='./output/log/train_{}.log'.format(ts))
+
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('cat classification', add_help=False)
+    parser.add_argument('--class_num', default=3, type=int)
+    parser.add_argument('--optimizer', default='Adam', type=str, help='optimizer for training')
+    parser.add_argument('--base_lr', default=1e-3, type=float)
+    parser.add_argument('--momentum', default=0.9, type=float)
+    parser.add_argument('--batch_size', default=64, type=int)
+    parser.add_argument('--weight_decay', default=1e-4, type=float)
+    parser.add_argument('--epochs', default=500, type=int)
+    parser.add_argument('--lr_drop', default=200, type=int)
+    parser.add_argument('--num_workers', default=4, type=int)
+    parser.add_argument('--start_epoch', default=0, type=int)
+    parser.add_argument('--clip_max_norm', default=0.1, type=float, help='gradient clipping max norm')
+    parser.add_argument('--backbone', default='resnet18', type=str)
+    parser.add_argument('--eval', default=False, type=bool)
+    parser.add_argument('--device', default='cuda', type=str, help='device to use for training / testing')
+    parser.add_argument('--resume', default='', type=str, help='resume model path')
+    parser.add_argument('--output_dir', default='output', type=str, help='path where to save, empty for no saving')
+    parser.add_argument('--gpu_list', default='1', type=str, help='gpu list for using')
+    parser.add_argument('--pretrained_model', default='pretrain/resnet18-5c106cde.pth', type=str, help='pretrain model for using')
+    parser.add_argument('--data_path', default='../data/train', type=str, help='path for training data')
+    return parser
+
+
+def build_data_loader(args):
+    logger.info("build train dataset")
+    train_dataset = data_reader.data_reader(args.data_path)
+    logger.info("build dataset done")
+
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, num_workers=1, shuffle=True)
+    return train_loader
+
+
+def train_one_epoch(model, data_loader, lr_scheduler, optimizer,
+                    device, epoch, max_norm = 0):
+
+    for idx, (samples, targets) in enumerate(data_loader):
+        samples = samples.to(device)
+        targets = targets.to(device)
+
+        losses = model([samples, targets])
+
+        optimizer.zero_grad()
+        losses.backward()
+        if max_norm > 0:
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm)
+        optimizer.step()
+        if idx % 100 == 0:
+            info = "Epoch: [{}][{}/{}] loss: {:.4f} lr: {:.6f}".format(
+                                epoch+1, (idx+1) % len(data_loader),
+                                len(data_loader), losses, optimizer.state_dict()['param_groups'][0]['lr'])
+            logger.info(info)
+    return
+
+def main(args):
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_list
+
+    device = torch.device(args.device)
+    logger.info(device)
+
+    # create data loader
+    train_loader = build_data_loader(args)
+
+    # build model
+    model = model_build.build(args)
+
+    # optimizer and lr_scheduler
+    if args.optimizer == 'SGD':
+        optimizer = torch.optim.SGD(model.parameters(),
+                                    lr=args.base_lr,
+                                    momentum=args.momentum,
+                                    weight_decay=args.weight_decay)
+    elif args.optimizer == 'Adam':
+        optimizer = torch.optim.Adam(model.parameters(),
+                                    lr=args.base_lr,
+                                    weight_decay=args.weight_decay)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, args.lr_drop, gamma=0.1)
+
+    # resume
+    if args.resume:
+        print('resume from {}'.format(args.resume))
+        checkpoint = torch.load(args.resume, map_location='cpu')
+        model.load_state_dict(checkpoint['model'])
+        if not args.eval and 'optimizer' in checkpoint and 'lr_scheduler' in checkpoint and 'epoch' in checkpoint:
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
+            args.start_epoch = checkpoint['epoch'] + 1
+
+    # start training
+    start_time = time.time()
+    for epoch in range(args.start_epoch, args.epochs):
+        train_stats = train_one_epoch(
+            model, train_loader, lr_scheduler, optimizer, device, epoch,
+            args.clip_max_norm)
+        lr_scheduler.step()
+        if args.output_dir:
+            if not os.path.isdir(args.output_dir):
+                os.system('mkdir {}'.format(args.output_dir))
+            if not os.path.isdir(osp.join(args.output_dir, ts)):
+                os.system('mkdir {}/{}'.format(args.output_dir, ts))
+            if (epoch + 1) % 10 == 0:
+                checkpoint_path = osp.join(args.output_dir, ts, 'checkpoint{}.pth'.format('%05d'%epoch))
+                 # extra checkpoint before LR drop and every 100 epochs
+                torch.save({
+                    'model': model.state_dict(),
+                    'optimizer': optimizer.state_dict(),
+                    'lr_scheduler': lr_scheduler.state_dict(),
+                    'epoch': epoch,
+                    'args': args,
+                }, checkpoint_path)
+
+        # test_stats, coco_evaluator = evaluate(
+        #     model, criterion, postprocessors, data_loader_val, base_ds, device, args.output_dir
+        # )
+
+        # log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
+        #              **{f'test_{k}': v for k, v in test_stats.items()},
+        #              'epoch': epoch,
+        #              'n_parameters': n_parameters}
+
+        # if args.output_dir and utils.is_main_process():
+        #     with (output_dir / "log.txt").open("a") as f:
+        #         f.write(json.dumps(log_stats) + "\n")
+
+    total_time = time.time() - start_time
+    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+    logger.info('Training time {}'.format(total_time_str))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('qrcode segmentation training and evaluation script', parents=[get_args_parser()])
+    args = parser.parse_args()
+    # if args.output_dir:
+    #     Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+    main(args)
