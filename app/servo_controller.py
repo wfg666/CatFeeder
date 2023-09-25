8a6fc10199cdbdb40402105382e332dc5e2d14e3
wangfugui <mahgix@qq.com>
2023-09-26 00:33:02 +0800

    形态由掉粮改为开门（更改舵机动作），加了几张猫图

--- app/servo_controller.py
+++ app/servo_controller.py
@@ -3,24 +3,60 @@ import time
 
 ser = None
 
-def feed():
+def open_serial():
     global ser
     if(not ser):
         try:
             ser=serial.Serial("/dev/ttyTHS1",115200,timeout=0)
         except Exception as e:
             print("串口初始化出错 ",e)
+
+def feed():
+    global ser
+    if(not ser):
+        open_serial()
     
     try:
         ser.write(bytearray([0xFF,]))
         time.sleep(0.8)
         ser.write(bytearray([0x50,]))
-        # time.sleep(0.3)
-        # ser.write(bytearray([0xFF,]))
-        # time.sleep(0.8)
-        # ser.write(bytearray([0x50,]))
+
     except Exception as e:
         print("发送失败 ",e)
 
+def open_door():
+    global ser
+    if(not ser):
+        open_serial()
+    
+    for i in range(0x55,0xFD,1):
+        ser.write(bytearray([i,]))
+        time.sleep(0.01)
+
+    time.sleep(6)
+    for i in range(0xfD,0x55,-1):
+        ser.write(bytearray([i,]))
+        time.sleep(0.01)
+
+    # try:
+    #     ser.write(bytearray([0x6F,]))
+    #     time.sleep(0.1)
+    #     ser.write(bytearray([0x8F,]))
+    #     time.sleep(0.1)
+    #     ser.write(bytearray([0xAF,]))
+    #     time.sleep(0.1)
+    #     ser.write(bytearray([0xCF,]))
+    #     time.sleep(0.1)
+    #     ser.write(bytearray([0xEF,]))
+    #     time.sleep(0.1)
+    #     ser.write(bytearray([0xFF,]))
+    #     time.sleep(6)
+    #     ser.write(bytearray([0x85,]))
+    #     time.sleep(0.2)
+    #     ser.write(bytearray([0x55,]))
+
+    # except Exception as e:
+    #     print("发送失败 ",e)
+
 if __name__ == "__main__":
-    feed()
+    open_door()
