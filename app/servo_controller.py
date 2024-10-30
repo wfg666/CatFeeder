eaeb8b7b251259fd92631781ece5aba0aff4d07a
mahaocheng <reg@thu.news>
2024-10-31 01:24:19 +0800

    舵机改为飞特串口舵机

--- app/servo_controller.py
+++ app/servo_controller.py
@@ -3,60 +3,45 @@ import time
 
 ser = None
 
-def open_serial():
+def int16_to_sb_feetech_int16(x):
+    if x < 0 :
+        return ((0x8000 | -x) & 0xFFFF).to_bytes(2, byteorder='little', signed=False) 
+    else :
+        return x.to_bytes(2, byteorder='little', signed=True)
+
+
+def set_pos(pos):
     global ser
     if(not ser):
         try:
             ser=serial.Serial("/dev/ttyTHS1",115200,timeout=0)
+            # ser=serial.Serial("/dev/ttyUSB0",115200,timeout=0)
         except Exception as e:
             print("串口初始化出错 ",e)
 
-def feed():
-    global ser
-    if(not ser):
-        open_serial()
-    
-    try:
-        ser.write(bytearray([0xFF,]))
-        time.sleep(0.8)
-        ser.write(bytearray([0x50,]))
 
+    pkt = bytearray([0xFF, 0xFF, 0xFE, 13, 0x83, 0x2A, 2, 1, 0, 0, 2, 0, 0, 3, 0, 0])
+
+    pkt[8:10] = int16_to_sb_feetech_int16(pos)
+    pkt[11:13] = int16_to_sb_feetech_int16(pos)
+    pkt[14:16] = int16_to_sb_feetech_int16(pos)
+
+    cs = 0
+    for i in range(2, pkt[3] + 3):
+        cs += pkt[i]
+    pkt.append(~cs & 0xFF)  # Append checksum
+
+    try:
+        ser.write(pkt)
     except Exception as e:
         print("发送失败 ",e)
+        
 
 def open_door():
-    global ser
-    if(not ser):
-        open_serial()
-    
-    for i in range(0x55,0xFD,1):
-        ser.write(bytearray([i,]))
-        time.sleep(0.01)
+    set_pos(2240)
+    time.sleep(7)
+    set_pos(3200)
 
-    time.sleep(6)
-    for i in range(0xfD,0x55,-1):
-        ser.write(bytearray([i,]))
-        time.sleep(0.01)
-
-    # try:
-    #     ser.write(bytearray([0x6F,]))
-    #     time.sleep(0.1)
-    #     ser.write(bytearray([0x8F,]))
-    #     time.sleep(0.1)
-    #     ser.write(bytearray([0xAF,]))
-    #     time.sleep(0.1)
-    #     ser.write(bytearray([0xCF,]))
-    #     time.sleep(0.1)
-    #     ser.write(bytearray([0xEF,]))
-    #     time.sleep(0.1)
-    #     ser.write(bytearray([0xFF,]))
-    #     time.sleep(6)
-    #     ser.write(bytearray([0x85,]))
-    #     time.sleep(0.2)
-    #     ser.write(bytearray([0x55,]))
-
-    # except Exception as e:
-    #     print("发送失败 ",e)
 
 if __name__ == "__main__":
     open_door()
